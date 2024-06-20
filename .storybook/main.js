83f31d7990fdaffd21f8b735386f1f1adb81a559
Ravi Sundram <ravisundram@gmail.com>
2024-06-21 09:30:32 +1000

    feat: initial repository

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+   '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+};
+export default config;
---
+++ .storybook/preview.js
@@ -0,0 +1,25 @@
+
+import { setup } from '@storybook/vue3';
+
+import { createPinia } from 'pinia';
+
+import '../src/index.css';
+
+//👇 Registers a global Pinia instance inside Storybook to be consumed by existing stories
+setup((app) => {
+  app.use(createPinia());
+});
+
+/** @type { import('@storybook/vue3').Preview } */
+const preview = {
+ parameters: {
+   controls: {
+     matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/,
+     },
+   },
+ },
+};
+
+export default preview;
